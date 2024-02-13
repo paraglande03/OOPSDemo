dcb4c148c3dbb6672f2d574332d57b038d53ac39
Parag Lande <parag.lande@ibm.com>
2024-02-14 00:55:28 +0530

    Added: initial commit

---
+++ src/Inheritance.java
@@ -0,0 +1,33 @@
+import java.util.zip.ZipOutputStream;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        // Hiding the implementation showing the declaration to consumer
+
+        factoryInterface f = new FactoryImplementation();
+
+        f.getIceCream("Vanilla");
+        String result  = f.getIceCream("asd");
+        f.getIceCreamByName("name");
+
+    }
+
+}
+
+
+interface factoryInterface{
+    public String getIceCream(String a);
+    public void getIceCreamByName(String abc);
+}
+
+class FactoryImplementation implements factoryInterface{
+
+    public String getIceCream(String name){
+       return  "Enjoy your IceCream"+name;
+    }
+    public void getIceCreamByName(String name){
+        System.out.println("Enjoy your IceCream"+name);
+    }
+}
